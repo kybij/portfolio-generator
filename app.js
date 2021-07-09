0c83f24eacbd328cde70f36a17f8e4d49bcddda3
Kylee Bijerring <kylee24b@gmail.com>
2021-07-08 19:48:42 -0600

    new

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer')
+const promptUser = () => {
+return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+};
+  promptUser().then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-const pageHTML = generatePage(name, github)
+// const pageHTML = generatePage(name, github)
 
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-}); 
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// }); 
